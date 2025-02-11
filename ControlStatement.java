fefc094c3f0c59ab8339b106ed5f46a11ecbe58f
SivaTrainner <mscsiva7@gmail.com>
2025-02-11 09:51:52 +0530

    checkingcontrol stat
    control statement

---
+++ ControlStatement.java
@@ -0,0 +1,19 @@
+package week2.day1;
+
+public class ControlStatement {
+
+	public static void main(String[] args) {
+		int x=8;
+		int y=12;
+		
+		// TODO Auto-generated method stub
+		int income = 8924;
+		if (income < 0) {
+			System.out.println("If else checking");
+		}else if(income < 8925){
+			System.out.println("sample if else checking");
+		}
+
+	}
+
+}
